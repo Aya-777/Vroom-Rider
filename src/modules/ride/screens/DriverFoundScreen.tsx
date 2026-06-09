d72582de6b488fde152ea998aff71adca03c60df
Aya yassien <yassienaya714@gmail.com>
2026-06-10 01:47:09 +0300

    driver found nav

--- src/modules/ride/screens/DriverFoundScreen.tsx
+++ src/modules/ride/screens/DriverFoundScreen.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { View, StatusBar } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-
 import { useTheme } from '../../../core/theme/useTheme';
-import { HomeStackScreenProps } from '../../../navigation/main/home/homeTypes';
 import BottomSheetCard from '../../../shared/components/ride/BottomSheetCard';
 import Header from '../../../shared/components/ride/Header';
-
 import { useDriverHereViewModel } from '../viewmodels/useDriverHereViewModel';
 import { createStyles } from '../styles/DriverFound.styles';
-
 import DriverStatus from '../components/DriverFoundScreen/DriverStatus';
 import DriverAvatar from '../components/DriverFoundScreen/DriverAvatar';
 import CommunicationActions from '../components/DriverFoundScreen/CommunicationActions';
@@ -19,14 +14,13 @@ import ProgressBar from '../components/DriverFoundScreen/ProgressBar';
 export default function DriverFoundScreen() {
   const { colors } = useTheme();
   const styles = createStyles(colors);
-  const navigation = useNavigation<HomeStackScreenProps<'DriverFound'>['navigation']>();
 
-  const { driver } = useDriverHereViewModel();
+  const { driver, handleBackPress } = useDriverHereViewModel();
 
   return (
     <View style={styles.contentContainer}>
       <StatusBar translucent backgroundColor="transparent" />
-      <Header title="Track your trip" onBackPress={() => navigation.goBack()} />
+      <Header title="Track your trip" onBackPress={handleBackPress} />
 
       <BottomSheetCard>
         <DriverStatus text={driver.statusMessage} styles={styles} />
