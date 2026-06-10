923cb54fdb24b977289a78c42c001bba8b726941
Aya yassien <yassienaya714@gmail.com>
2026-06-10 21:10:15 +0300

    containing all lang files in resources

--- src/core/i18n/resources.ts
+++ src/core/i18n/resources.ts
@@ -1,12 +1,48 @@
 import authEn from './locales/auth/en.json';
 import authAr from './locales/auth/ar.json';
+import homeEn from './locales/home/ar.json';
+import homeAr from './locales/home/ar.json';
+import navEn from './locales/navigation/en.json'
+import navAr from './locales/navigation/ar.json'
+import profileEn from './locales/profile/en.json'
+import profileAr from './locales/profile/ar.json'
+import selectRideEn from './locales/ride/selectRide/en.json'
+import selectRideAr from './locales/ride/selectRide/ar.json'
+import rideDetailsAr from './locales/ride/rideDetails/ar.json'
+import rideDetailsEn from './locales/ride/rideDetails/en.json'
+import confirmRideAr from './locales/ride/confirmRide/ar.json'
+import confirmRideEn from './locales/ride/confirmRide/en.json'
+import driverFoundAr from './locales/ride/driverFound/ar.json'
+import driverFoundEn from './locales/ride/driverFound/en.json'
+import driverHereAr from './locales/ride/driverHere/ar.json'
+import driverHereEn from './locales/ride/driverHere/en.json'
+import reviewAr from './locales/ride/review/ar.json'
+import reviewEn from './locales/ride/review/en.json'
 
 export const resources = {
   en: {
     auth: authEn,
+    home: homeEn,
+    selectRide: selectRideEn,
+    rideDetails: rideDetailsEn,
+    confirmRide: confirmRideEn,
+    driverHere: driverHereEn,
+    driverFound: driverFoundEn,
+    review: reviewEn,
+    nav: navEn,
+    profile: profileEn
   },
-
+  
   ar: {
     auth: authAr,
+    home: homeAr,
+    selectRide: selectRideAr,
+    rideDetails: rideDetailsAr,
+    confirmRide: confirmRideAr,
+    driverHere: driverHereAr,
+    driverFound: driverFoundAr,
+    review: reviewAr,
+    nav: navAr,
+    profile: profileAr
   },
 };
