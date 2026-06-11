0c96976eae1da0980fa3bedf8c68dec43f2e76d9
Masa Hammoud <masahammoud2005@gmail.com>
2026-06-11 18:14:06 +0300

    Validation inputs

--- src/modules/auth/components/signup/SignupForm.tsx
+++ src/modules/auth/components/signup/SignupForm.tsx
@@ -1,7 +1,211 @@
+// import React from 'react';
+// import {
+//     View,
+//     TextInput,
+//     TouchableOpacity,
+//     Text,
+// } from 'react-native';
+// import Logo from '../shared/logo';
+// import LinearBg from '../../../../shared/components/LinearBg';
+// import Profile from '../../../../assets/svg/profile/profile.svg';
+// import PhoneNumberIcon from '../../../../assets/svg/contact/call.svg';
+// import PasswordIcon from '../../../../assets/svg/common/password.svg';
+// import VisibilityOnIcon from '../../../../assets/svg/common/visibilityOn.svg';
+// import VisibilityOffIcon from '../../../../assets/svg/common/visibilityOff.svg';
+// import CameraIcon from '../../../../assets/svg/common/camera.svg';
+// import { useTheme } from '../../../../core/theme/useTheme';
+// import { createStyles } from '../../styles/signup.styles';
+
+// const SignupForm = ({ vm }: any) => {
+//     const { colors } = useTheme();
+//     const styles = createStyles(colors);
+
+//     return (
+//         <View>
+
+//             <View style={styles.top}>
+//                 <View style={styles.logoWrapper}>
+//                     <Logo type="signup" />
+
+//                     <TouchableOpacity style={styles.cameraButton}>
+//                         <View style={styles.cameraCircle}>
+//                             <CameraIcon
+//                                 width={16}
+//                                 height={16}
+//                                 fill={colors.primary}
+//                             />
+//                         </View>
+//                     </TouchableOpacity>
+//                 </View>
+//             </View>
+
+//             {/* FORM */}
+//             <View style={styles.middle}>
+//                 {/* First + Last Name */}
+//                 <View style={styles.row}>
+//                     <View
+//                         style={[
+//                             styles.halfInput,
+//                         ]}
+//                     >
+//                         <Profile width={18} height={18} fill={colors.primary} />
+
+//                         <TextInput
+//                             placeholder="First Name"
+//                             placeholderTextColor={colors.textMuted}
+//                             value={vm.firstName}
+//                             onChangeText={vm.setFirstName}
+//                             style={[
+//                                 styles.halfInputText,
+//                                 { color: colors.textPrimary },
+//                             ]}
+//                         />
+//                     </View>
+
+//                     <View
+//                         style={[
+//                             styles.halfInput,
+//                         ]}
+//                     >
+//                         <Profile width={18} height={18} fill={colors.primary} />
+
+//                         <TextInput
+//                             placeholder="Last Name"
+//                             placeholderTextColor={colors.textMuted}
+//                             value={vm.lastName}
+//                             onChangeText={vm.setLastName}
+//                             style={[
+//                                 styles.halfInputText,
+//                                 { color: colors.textPrimary },
+//                             ]}
+//                         />
+//                     </View>
+//                 </View>
+
+//                 {/* Phone */}
+//                 <View
+//                     style={[
+//                         styles.inputBox,
+//                     ]}
+//                 >
+//                     <PhoneNumberIcon width={20} height={20} fill={colors.primary} />
+
+//                     <TextInput
+//                         placeholder="Phone Number"
+//                         placeholderTextColor={colors.textMuted}
+//                         keyboardType="phone-pad"
+//                         value={vm.phone}
+//                         onChangeText={vm.setPhone}
+//                         style={[
+//                             styles.input,
+//                             { color: colors.textPrimary },
+//                         ]}
+//                     />
+//                 </View>
+
+//                 {/* Password */}
+//                 <View
+//                     style={[
+//                         styles.inputBox,
+//                     ]}
+//                 >
+//                     <PasswordIcon width={20} height={20} fill={colors.primary} />
+
+//                     <TextInput
+//                         placeholder="Password"
+//                         placeholderTextColor={colors.textMuted}
+//                         secureTextEntry={!vm.showPassword}
+//                         value={vm.password}
+//                         onChangeText={vm.setPassword}
+//                         style={[
+//                             styles.input,
+//                             { color: colors.textPrimary },
+//                         ]}
+//                     />
+
+//                     <TouchableOpacity
+//                         onPress={vm.togglePassword}
+//                     >
+//                         {vm.showPassword ? (
+//                             <VisibilityOnIcon
+//                                 width={20}
+//                                 height={20}
+//                                 fill={colors.primary}
+//                             />
+//                         ) : (
+//                             <VisibilityOffIcon
+//                                 width={20}
+//                                 height={20}
+//                                 fill={colors.primary}
+//                             />
+//                         )}
+//                     </TouchableOpacity>
+//                 </View>
+
+//                 {/* Confirm Password */}
+//                 <View
+//                     style={[
+//                         styles.inputBox,
+//                     ]}
+//                 >
+//                     <PasswordIcon width={20} height={20} fill={colors.primary} />
+
+//                     <TextInput
+//                         placeholder="Confirm Password"
+//                         placeholderTextColor={colors.textMuted}
+//                         secureTextEntry={
+//                             !vm.showConfirmPassword
+//                         }
+//                         value={vm.confirmPassword}
+//                         onChangeText={vm.setConfirmPassword}
+//                         style={[
+//                             styles.input,
+//                             { color: colors.textPrimary },
+//                         ]}
+//                     />
+
+//                     <TouchableOpacity
+//                         onPress={vm.toggleConfirmPassword}
+//                     >
+//                         {vm.showConfirmPassword ? (
+//                             <VisibilityOnIcon
+//                                 width={20}
+//                                 height={20}
+//                                 fill={colors.primary}
+//                             />
+//                         ) : (
+//                             <VisibilityOffIcon
+//                                 width={20}
+//                                 height={20}
+//                                 fill={colors.primary}
+//                             />
+//                         )}
+//                     </TouchableOpacity>
+//                 </View>
+//             </View>
+
+//             {/* BUTTON */}
+//             <View style={styles.bottom}>
+//                 <TouchableOpacity onPress={vm.handleLogin}>
+//                     <LinearBg
+//                         style={styles.button}
+//                         colors={[colors.textPrimary, colors.surface]}
+//                     >
+//                         <Text style={[styles.btnText, { color: colors.backgroundSoft }]}>
+//                             SignUp
+//                         </Text>
+//                     </LinearBg>
+//                 </TouchableOpacity>
+//             </View>
+//         </View>
+//     );
+// };
+
+// export default SignupForm;
+
 import React from 'react';
 import {
     View,
-    TextInput,
     TouchableOpacity,
     Text,
 } from 'react-native';
@@ -15,6 +219,7 @@ import VisibilityOffIcon from '../../../../assets/svg/common/visibilityOff.svg';
 import CameraIcon from '../../../../assets/svg/common/camera.svg';
 import { useTheme } from '../../../../core/theme/useTheme';
 import { createStyles } from '../../styles/signup.styles';
+import Input from '../../../../shared/components/Input';
 
 const SignupForm = ({ vm }: any) => {
     const { colors } = useTheme();
@@ -22,10 +227,7 @@ const SignupForm = ({ vm }: any) => {
 
     return (
         <View>
-            {/* LOGO */}
-            {/* <View style={styles.top}>
-                <Logo type="signup" />
-            </View> */}
+
             <View style={styles.top}>
                 <View style={styles.logoWrapper}>
                     <Logo type="signup" />
@@ -42,152 +244,95 @@ const SignupForm = ({ vm }: any) => {
                 </View>
             </View>
 
-            {/* FORM */}
             <View style={styles.middle}>
-                {/* First + Last Name */}
+
                 <View style={styles.row}>
-                    <View
-                        style={[
-                            styles.halfInput,
-                        ]}
-                    >
-                        <Profile width={18} height={18} fill={colors.primary} />
-
-                        <TextInput
-                            placeholder="First Name"
-                            placeholderTextColor={colors.textMuted}
-                            value={vm.firstName}
-                            onChangeText={vm.setFirstName}
-                            style={[
-                                styles.halfInputText,
-                                { color: colors.textPrimary },
-                            ]}
-                        />
-                    </View>
-
-                    <View
-                        style={[
-                            styles.halfInput,
-                        ]}
-                    >
-                        <Profile width={18} height={18} fill={colors.primary} />
-
-                        <TextInput
-                            placeholder="Last Name"
-                            placeholderTextColor={colors.textMuted}
-                            value={vm.lastName}
-                            onChangeText={vm.setLastName}
-                            style={[
-                                styles.halfInputText,
-                                { color: colors.textPrimary },
-                            ]}
-                        />
-                    </View>
-                </View>
-
-                {/* Phone */}
-                <View
-                    style={[
-                        styles.inputBox,
-                    ]}
-                >
-                    <PhoneNumberIcon width={20} height={20} fill={colors.primary} />
-
-                    <TextInput
-                        placeholder="Phone Number"
+                    <Input
+                        type="text"
+                        placeholder="First Name"
                         placeholderTextColor={colors.textMuted}
-                        keyboardType="phone-pad"
-                        value={vm.phone}
-                        onChangeText={vm.setPhone}
-                        style={[
-                            styles.input,
-                            { color: colors.textPrimary },
-                        ]}
-                    />
-                </View>
-
-                {/* Password */}
-                <View
-                    style={[
-                        styles.inputBox,
-                    ]}
-                >
-                    <PasswordIcon width={20} height={20} fill={colors.primary} />
-
-                    <TextInput
-                        placeholder="Password"
-                        placeholderTextColor={colors.textMuted}
-                        secureTextEntry={!vm.showPassword}
-                        value={vm.password}
-                        onChangeText={vm.setPassword}
-                        style={[
-                            styles.input,
-                            { color: colors.textPrimary },
-                        ]}
-                    />
-
-                    <TouchableOpacity
-                        onPress={vm.togglePassword}
-                    >
-                        {vm.showPassword ? (
-                            <VisibilityOnIcon
-                                width={20}
-                                height={20}
-                                fill={colors.primary}
-                            />
-                        ) : (
-                            <VisibilityOffIcon
-                                width={20}
-                                height={20}
-                                fill={colors.primary}
-                            />
+                        value={vm.firstName}
+                        onChangeText={vm.setFirstName}
+                        containerStyle={styles.halfInput}
+                        inputStyle={[styles.halfInputText, { color: colors.textPrimary }]}
+                        renderLeftIcon={() => (
+                            <Profile width={18} height={18} fill={colors.primary} />
                         )}
-                    </TouchableOpacity>
-                </View>
-
-                {/* Confirm Password */}
-                <View
-                    style={[
-                        styles.inputBox,
-                    ]}
-                >
-                    <PasswordIcon width={20} height={20} fill={colors.primary} />
-
-                    <TextInput
-                        placeholder="Confirm Password"
-                        placeholderTextColor={colors.textMuted}
-                        secureTextEntry={
-                            !vm.showConfirmPassword
-                        }
-                        value={vm.confirmPassword}
-                        onChangeText={vm.setConfirmPassword}
-                        style={[
-                            styles.input,
-                            { color: colors.textPrimary },
-                        ]}
+                        error={vm.firstNameError}
                     />
 
-                    <TouchableOpacity
-                        onPress={vm.toggleConfirmPassword}
-                    >
-                        {vm.showConfirmPassword ? (
-                            <VisibilityOnIcon
-                                width={20}
-                                height={20}
-                                fill={colors.primary}
-                            />
-                        ) : (
-                            <VisibilityOffIcon
-                                width={20}
-                                height={20}
-                                fill={colors.primary}
-                            />
+                    <Input
+                        type="text"
+                        placeholder="Last Name"
+                        placeholderTextColor={colors.textMuted}
+                        value={vm.lastName}
+                        onChangeText={vm.setLastName}
+                        containerStyle={styles.halfInput}
+                        inputStyle={[styles.halfInputText, { color: colors.textPrimary }]}
+                        renderLeftIcon={() => (
+                            <Profile width={18} height={18} fill={colors.primary} />
                         )}
-                    </TouchableOpacity>
+                        error={vm.lastNameError}
+                    />
                 </View>
+
+                <Input
+                    type="phone"
+                    placeholder="Phone Number"
+                    placeholderTextColor={colors.textMuted}
+                    value={vm.phone}
+                    onChangeText={vm.setPhone}
+                    containerStyle={styles.inputBox}
+                    inputStyle={[{ color: colors.textPrimary }]}
+                    renderLeftIcon={() => (
+                        <PhoneNumberIcon width={20} height={20} fill={colors.primary} />
+                    )}
+                    error={vm.phoneError}
+                />
+
+                <Input
+                    type="password"
+                    placeholder="Password"
+                    placeholderTextColor={colors.textMuted}
+                    value={vm.password}
+                    onChangeText={vm.setPassword}
+                    containerStyle={styles.inputBox}
+                    inputStyle={[{ color: colors.textPrimary }]}
+                    renderLeftIcon={() => (
+                        <PasswordIcon width={20} height={20} fill={colors.primary} />
+                    )}
+                    renderRightIcon={(isPasswordVisible) =>
+                        isPasswordVisible ? (
+                            <VisibilityOnIcon width={20} height={20} fill={colors.primary} />
+                        ) : (
+                            <VisibilityOffIcon width={20} height={20} fill={colors.primary} />
+                        )
+                    }
+                    error={vm.passwordError}
+                />
+
+                <Input
+                    type="password"
+                    placeholder="Confirm Password"
+                    placeholderTextColor={colors.textMuted}
+                    value={vm.confirmPassword}
+                    onChangeText={vm.setConfirmPassword}
+                    containerStyle={styles.inputBox}
+                    inputStyle={[{ color: colors.textPrimary }]}
+                    renderLeftIcon={() => (
+                        <PasswordIcon width={20} height={20} fill={colors.primary} />
+                    )}
+                    renderRightIcon={(isPasswordVisible) =>
+                        isPasswordVisible ? (
+                            <VisibilityOnIcon width={20} height={20} fill={colors.primary} />
+                        ) : (
+                            <VisibilityOffIcon width={20} height={20} fill={colors.primary} />
+                        )
+                    }
+                    error={vm.confirmPasswordError}
+                />
             </View>
 
-            {/* BUTTON */}
             <View style={styles.bottom}>
                 <TouchableOpacity onPress={vm.handleLogin}>
                     <LinearBg
--- src/shared/components/Input.tsx
+++ src/shared/components/Input.tsx
@@ -1,29 +1,120 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   TextInput,
   TextInputProps,
   Text,
   View,
   StyleSheet,
+  TouchableOpacity,
+  StyleProp,
+  ViewStyle,
+  TextStyle,
 } from 'react-native';
 import { Radius } from '../../core/theme/tokens/radius';
 import { Typography } from '../../core/theme/tokens/typography';
 
-interface InputProps extends TextInputProps {
+type InputType = 'text' | 'phone' | 'password';
+
+interface InputProps extends Omit<TextInputProps, 'style'> {
+  type?: InputType;
   error?: string;
+  containerStyle?: StyleProp<ViewStyle>;
+  inputStyle?: StyleProp<TextStyle>;
+  renderLeftIcon?: () => React.ReactNode;
+  renderRightIcon?: (isPasswordVisible?: boolean) => React.ReactNode;
 }
 
-export default function Input({ error, style, ...props }: InputProps) {
+export default function Input({
+  type = 'text',
+  error,
+  containerStyle,
+  inputStyle,
+  onChangeText,
+  secureTextEntry,
+  keyboardType,
+  renderLeftIcon,
+  renderRightIcon,
+  ...props
+}: InputProps) {
+  const [isPasswordVisible, setIsPasswordVisible] = useState(false);
+
+  const handleTextChange = (text: string) => {
+    if (type === 'phone') {
+      const cleaned = text.replace(/[^0-9]/g, '');
+      const limited = cleaned.slice(0, 10);
+      onChangeText?.(limited);
+    } else {
+      onChangeText?.(text);
+    }
+  };
+
+  const finalMaxLength = type === 'phone' ? 10 : props.maxLength;
+
+  const isPassword = type === 'password';
+  const finalSecureTextEntry = isPassword ? !isPasswordVisible : secureTextEntry;
+  const finalKeyboardType = type === 'phone' ? 'numeric' : keyboardType;
+
   return (
-    <View style={styles.container}>
-      <TextInput style={[error ? styles.inputError : null, style]} {...props} />
+    //     <View style={styles.container}>
+    //       <TextInput style={[error ? styles.inputError : null, style]} {...props} />
+    //       {error && <Text style={styles.errorText}>{error}</Text>}
+    //     </View>
+    <View style={[styles.container, containerStyle]}>
+
+      <View style={[styles.inputBox, error ? styles.inputError : null]}>
+
+        {renderLeftIcon && renderLeftIcon()}
+
+        <TextInput
+          style={[styles.defaultInput, inputStyle]}
+          onChangeText={handleTextChange}
+          secureTextEntry={finalSecureTextEntry}
+          keyboardType={finalKeyboardType}
+          maxLength={finalMaxLength}
+          {...props}
+        />
+
+        {isPassword && renderRightIcon && (
+          <TouchableOpacity onPress={() => setIsPasswordVisible(!isPasswordVisible)}>
+            {renderRightIcon(isPasswordVisible)}
+          </TouchableOpacity>
+        )}
+      </View>
+
       {error && <Text style={styles.errorText}>{error}</Text>}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+
+  defaultContainer: {
+    marginBottom: 15,
+    width: '100%',
+  },
+  inputBox: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+  },
+  defaultInput: {
+    flex: 1,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+  },
+  //   container: {},
+  //   inputError: { borderColor: 'red', borderWidth: 0.5, borderRadius: Radius.sm },
+  //   errorText: { color: 'red', ...Typography.smallCaption },
   container: {},
-  inputError: { borderColor: 'red', borderWidth: 0.5, borderRadius: Radius.sm },
-  errorText: { color: 'red', ...Typography.smallCaption },
+
+  inputError: {
+    borderColor: 'red',
+    borderWidth: 0.5,
+    borderRadius: Radius.sm,
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 4,
+    ...Typography.smallCaption,
+  },
 });
