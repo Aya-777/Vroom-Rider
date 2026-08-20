2f72b9a258d09b1f929d6de1bc87763ad98e3472
Aya yassien <yassienaya714@gmail.com>
2026-08-20 15:55:01 +0300

    add contact us navigation

--- src/modules/favoriteDrivers/screens/FavoriteDriversScreen.tsx
+++ src/modules/favoriteDrivers/screens/FavoriteDriversScreen.tsx
@@ -25,12 +25,12 @@ export const FavoriteDriversScreen: React.FC = () => {
       style={styles.container}
     >
       <Header title={t('favoriteDrivers.title')} onBackPress={vm.goBack} />
-      <LinearBg
+      {/* <LinearBg
         colors={[colors.backgroundSoft, colors.background]}
         start={{ x: 0, y: 0 }}
         end={{ x: 0, y: 1 }}
         style={styles.container}
-      >
+      > */}
         {vm.drivers.length === 0 ? (
           <View style={styles.textContainer}>
             <Text style={styles.favoriteTitle}>{t('favoriteDrivers.noFavoriteDrivers')}</Text>
@@ -53,7 +53,7 @@ export const FavoriteDriversScreen: React.FC = () => {
             )}
           />
         )}
-      </LinearBg>
-    </>
-  );
-};
+    {/* </LinearBg> */}
+    </LinearBg>
+  )
+}
